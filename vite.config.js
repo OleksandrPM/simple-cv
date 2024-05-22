fc0d3a5bbbde45fb1178606b9444241a09c53711
Oleksandr Moskaliuk <olekspm@gmail.com>
2024-05-22 21:42:29 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+
+export default {
+  base: "/moskaliuk-cv",
+  plugins: [
+    handlebars({
+      partialDirectory: [
+        resolve(__dirname, "./src/partials"),
+        resolve(__dirname, "./src/partials/components"),
+      ],
+    }),
+  ],
+};
